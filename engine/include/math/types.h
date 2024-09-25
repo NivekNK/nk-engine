1be3bbbc440e28187848e89f1697ea4a53a2a69b
NivekNK <kevin.rojas.v@mail.pucv.cl>
2024-09-25 17:21:08 -0300

    Removed own math library

--- engine/include/math/types.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace nk {
-    template<typename T>
-    concept NumericOrBoolean = std::is_arithmetic_v<T> || std::is_same_v<T, bool>;
-}
--- engine/include/math/vec2.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "math/vec/ivec2.h"
-
-namespace nk {
-    using f32vec2 = Ivec2<f32>;
-    using vec2 = f32vec2;
-}
