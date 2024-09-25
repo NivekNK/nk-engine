d171e1ef03a6c2d2613d37f7ddf8e438271626b9
NivekNK <kevin.rojas.v@mail.pucv.cl>
2024-09-25 18:22:16 -0300

    glm math library and working editor

--- editor/src/editor.cpp
+++ editor/src/editor.cpp
@@ -1,7 +1,5 @@
 #include <nk/app_creator.h>
 
-#include <math/vec2.h>
-
 NK_EXTEND_MEMORY_TYPE(Test)
 
 class Editor : public nk::App {
@@ -15,14 +13,6 @@ public:
     }) {
         NK_MEMORY_SYSTEM_EXPANDED_MEMORY_TYPE();
         DebugLog("Create Editor");
-
-        nk::vec2 v0(0.0f, 1.0f);
-
-        nk::vec2 v1(3.0f, 2.0f);
-
-        nk::vec2 result = v0 + v1;
-
-        DebugLog("Result: {}, {}", result.x, result.elements[1]);
     }
 
     virtual ~Editor() override {
