3ec4401b963f46b62ed65c059a965321643c9c45
NivekNK <kevin.rojas.v@mail.pucv.cl>
2025-06-24 23:19:51 -0400

    Depth Format, Logical Device, Queues and Command Pool

--- engine/src/renderer/vulkan/device.cpp
+++ engine/src/renderer/vulkan/device.cpp
@@ -38,14 +38,32 @@ namespace nk {
         if (!select_physical_device())
             return;
 
+        if (!detect_depth_format())
+            return;
+
+        create_logical_device();
+        obtain_queues();
+        create_command_pool();
+
         TraceLog("nk::Device initialized.");
     }
 
     void Device::shutdown() {
+        vkDestroyCommandPool(m_logical_device, m_graphics_command_pool, m_vulkan_allocator);
+        InfoLog("Vulkan Graphics Command Pool destroyed.");
+        
+        m_graphics_queue = nullptr;
+        m_present_queue = nullptr;
+        m_transfer_queue = nullptr;
+
+        vkDestroyDevice(m_logical_device, m_vulkan_allocator);
+        InfoLog("Vulkan Logical Device destroyed.");
+
         m_physical_device = nullptr;
 
         m_swapchain_support_info.formats.dyarr_shutdown();
         m_swapchain_support_info.present_modes.dyarr_shutdown();
+        InfoLog("SwapchainSupportInfo freed.");
 
         if (m_surface != nullptr) {
             vkDestroySurfaceKHR(m_instance->get(), m_surface, m_vulkan_allocator);
@@ -180,6 +198,109 @@ namespace nk {
 
         InfoLog("Vulkan Physical Device Selected.");
         return true;
+    }
+
+    bool Device::detect_depth_format() {
+        constexpr u64 candidate_count = 3;
+        VkFormat candidates[candidate_count] = {
+            VK_FORMAT_D32_SFLOAT,
+            VK_FORMAT_D32_SFLOAT_S8_UINT,
+            VK_FORMAT_D24_UNORM_S8_UINT,
+        };
+
+#if NK_DEV_MODE <= NK_RELEASE_DEBUG_INFO
+        constexpr cstr candidates_cstr[candidate_count] = {
+            "VK_FORMAT_D32_SFLOAT",
+            "VK_FORMAT_D32_SFLOAT_S8_UINT",
+            "VK_FORMAT_D24_UNORM_S8_UINT"};
+    #define _NK_GET_CANDIDATE(i) candidates_cstr[i]
+#else
+    #define _NK_GET_CANDIDATE(i) "INFO ONLY IN DEBUG MODE"
+#endif
+
+        u32 flags = VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT;
+        for (u64 i = 0; i < candidate_count; i++) {
+            VkFormatProperties properties;
+            vkGetPhysicalDeviceFormatProperties(m_physical_device, candidates[i], &properties);
+
+            if ((properties.linearTilingFeatures & flags) == flags) {
+                m_depth_format = candidates[i];
+                InfoLog("Vulkan Depth Format detected. Using: {}.", _NK_GET_CANDIDATE(i));
+                return true;
+            } else if ((properties.optimalTilingFeatures & flags) == flags) {
+                m_depth_format = candidates[i];
+                InfoLog("Vulkan Depth Format detected. Using: {}.", _NK_GET_CANDIDATE(i));
+                return true;
+            }
+        }
+
+        m_depth_format = VK_FORMAT_UNDEFINED;
+        ErrorLog("nk::Device::detect_depth_format Depth format not detected.");
+        return false;
+    }
+
+    void Device::create_logical_device() {
+        // NOTE: Do not create additional queues for shared indices.
+        constexpr u32 unique_queue_family_count = 3;
+        u32 unique_queue_families[unique_queue_family_count] = {
+            m_queue_family_info.graphics_family_index,
+            m_queue_family_info.present_family_index,
+            m_queue_family_info.transfer_family_index};
+
+        VkDeviceQueueCreateInfo queue_create_infos[unique_queue_family_count] = {};
+        u32 i = 0;
+        for (const u32 queue_family_index : unique_queue_families) {
+            queue_create_infos[i].sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+            queue_create_infos[i].queueFamilyIndex = queue_family_index;
+            queue_create_infos[i].queueCount = 1;
+
+            // TODO: Enable this for a future enhacement.
+            // if (indices[i] == m_queue_family_info.graphics_family_index)
+            //    queue_create_infos[i].queueCount = 2;
+
+            queue_create_infos[i].flags = 0;
+            queue_create_infos[i].pNext = nullptr;
+
+            f32 queue_priority = 1.0f;
+            queue_create_infos[i].pQueuePriorities = &queue_priority;
+            i++;
+        }
+
+        // Request device features.
+        // TODO: should be configurable.
+        VkPhysicalDeviceFeatures device_features = {};
+        device_features.samplerAnisotropy = VK_TRUE; // Request anistrophy
+
+        VkDeviceCreateInfo device_create_info = {VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO};
+        device_create_info.queueCreateInfoCount = unique_queue_family_count;
+        device_create_info.pQueueCreateInfos = queue_create_infos;
+        device_create_info.pEnabledFeatures = &device_features;
+        device_create_info.enabledExtensionCount = 1;
+        cstr extension_names = VK_KHR_SWAPCHAIN_EXTENSION_NAME;
+        device_create_info.ppEnabledExtensionNames = &extension_names;
+
+        // Deprecated and ignored, so pass nothing.
+        device_create_info.enabledLayerCount = 0;
+        device_create_info.ppEnabledLayerNames = nullptr;
+
+        // Create the device.
+        VulkanCheck(vkCreateDevice(m_physical_device, &device_create_info, m_vulkan_allocator, &m_logical_device));
+        InfoLog("Vulkan Logical Device created.");
+    }
+
+    void Device::obtain_queues() {
+        vkGetDeviceQueue(m_logical_device, m_queue_family_info.graphics_family_index, 0, &m_graphics_queue);
+        vkGetDeviceQueue(m_logical_device, m_queue_family_info.present_family_index, 0, &m_present_queue);
+        vkGetDeviceQueue(m_logical_device, m_queue_family_info.transfer_family_index, 0, &m_transfer_queue);
+        InfoLog("Vulkan Queues obtained.");
+    }
+
+    void Device::create_command_pool() {
+        VkCommandPoolCreateInfo pool_create_info = {VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO};
+        pool_create_info.queueFamilyIndex = m_queue_family_info.graphics_family_index;
+        pool_create_info.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+        VulkanCheck(vkCreateCommandPool(m_logical_device, &pool_create_info, m_vulkan_allocator, &m_graphics_command_pool));
+        InfoLog("Vulkan Graphics Command Pool created.");
     }
 
     bool physical_device_meets_requirements(
--- engine/src/renderer/vulkan/device.h
+++ engine/src/renderer/vulkan/device.h
@@ -48,13 +48,19 @@ namespace nk {
 
     private:
         bool select_physical_device();
+        bool detect_depth_format();
+        void create_logical_device();
+        void obtain_queues();
+        void create_command_pool();
 
         Instance* m_instance;
         mem::Allocator* m_allocator;
         VkAllocationCallbacks* m_vulkan_allocator;
 
+        // Surface
         VkSurfaceKHR m_surface;
 
+        // Physical Device
         VkPhysicalDevice m_physical_device;
         PhysicalDeviceQueueFamilyInfo m_queue_family_info;
         VkPhysicalDeviceProperties m_properties;
@@ -62,6 +68,18 @@ namespace nk {
         VkPhysicalDeviceMemoryProperties m_memory;
         SwapchainSupportInfo m_swapchain_support_info;
 
+        // Depth format
+        VkFormat m_depth_format;
+
+        // Logical Device
         VkDevice m_logical_device;
+
+        // Queues
+        VkQueue m_graphics_queue;
+        VkQueue m_present_queue;
+        VkQueue m_transfer_queue;
+
+        // Command pool
+        VkCommandPool m_graphics_command_pool;
     };
 }
